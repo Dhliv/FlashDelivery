db7971807ab8630ecbab6e99cc3a0b5df3f5cc44
ReynellQ <73651961+ReynellQ@users.noreply.github.com>
2021-04-18 16:04:56 -0500

    login funcional

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,5 +1,9 @@
 package controller;
 
+import model.*;
+
+import javax.swing.JOptionPane;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -7,8 +11,10 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 
-public class LoginController {
 
+public class LoginController {
+    private UsuarioDAO ud;
+    private EmpleadoDAO ed;
     @FXML
     private Button btIngresar;
 
@@ -20,7 +26,18 @@ public class LoginController {
 
     @FXML
     void clicksoide(ActionEvent event) {
-        System.out.println("XD");
+        ed = new EmpleadoDAO();
+        ud = new UsuarioDAO();
+        String user = txtUsuario.getText();
+        String pass = txtPass.getText();
+        int acc = ud.entradaUsuario(user, pass);
+        if(acc == -2){
+            JOptionPane.showMessageDialog(null, "Ud. no se encuentra habilitado en el sistema");
+        }else if(acc == -1){
+            JOptionPane.showMessageDialog(null, "No se ha encontrado su usuario");
+        }else{
+            JOptionPane.showMessageDialog(null, "Entraste!");
+        }
     }
 
 }
