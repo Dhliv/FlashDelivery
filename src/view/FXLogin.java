3ae7f38559dca9fb9d52fba4f0a5359e24090279
Dhliv <david.henao.martinez@correounivalle.edu.co>
2021-04-18 22:30:57 -0500

    implementacion de abrir ventanas segun el rol del usuario(semicompleto)

--- src/view/FXLogin.java
+++ src/view/FXLogin.java
@@ -7,18 +7,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class FXLogin extends Application{
+public class FXLogin extends Application {
     public static void main(String[] args) {
         launch(args);
     }
 
-
     @Override
     public void start(Stage stage) throws Exception {
         var loader = new FXMLLoader(getClass().getResource("login.fxml"));
-        loader.setController(new LoginController());
+        loader.setController(new LoginController(stage));
         Parent root = loader.load();
-        Scene scene = new Scene(root, 400, 275);
+        Scene scene = new Scene(root);
 
         stage.setTitle("FXML Welcome");
         stage.setScene(scene);
