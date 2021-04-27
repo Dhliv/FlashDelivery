d768914bb7793e0a9c0dd4bac911ecbaad828f5e
Dhliv <david.henao.martinez@correounivalle.edu.co>
2021-04-27 12:42:06 -0500

    Eliminación de números mágicos por representación global de roles.

--- src/Utilities/Roles.java
+++ src/Utilities/Roles.java
@@ -4,13 +4,23 @@ import java.util.ArrayList;
 
 public class Roles {
   public ArrayList<String> rol = new ArrayList<>();
+  public int ADMIN;
+  public int SECRETARIO;
+  public int OPERADOR;
+  public int AUXILIAR;
+  public int CONTADOR;
 
   public Roles() {
     rol.add("Gerente");
+    ADMIN = 0;
     rol.add("Secretario(a)");
+    SECRETARIO = 1;
     rol.add("Operador de oficina");
+    OPERADOR = 2;
     rol.add("Auxiliar");
-    rol.add("Vendedor de cocaina");
+    AUXILIAR = 3;
+    rol.add("Contador");
+    CONTADOR = 4;
   }
 
 }
