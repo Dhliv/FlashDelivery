93fc0fd8aee1c109abe398c972cd7da275c467e4
Dhliv <david.henao.martinez@correounivalle.edu.co>
2021-05-23 11:15:43 -0500

    .

---
+++ src/controller/OperadorConsulta.java
@@ -0,0 +1,23 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.Pane;
+
+public class OperadorConsulta implements Initializable {
+
+  @FXML private TableView<?> tablePaquetes;
+  @FXML private TableColumn<?, ?> columnDestinatario;
+  @FXML private TableColumn<?, ?> columnIdPaquete;
+  @FXML private TableColumn<?, ?> columnPesoPaquete;
+  @FXML private TableColumn<?, ?> columnEstado;
+
+  @Override public void initialize(URL location, ResourceBundle resources) {
+
+  }
+
+}
