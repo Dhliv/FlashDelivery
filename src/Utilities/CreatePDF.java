7f8d3629e1a6961b87107e5360665906dc6a11bf
Dhliv <david.henao.martinez@correounivalle.edu.co>
2021-05-28 10:51:40 -0500

    .

--- src/Utilities/CreatePDF.java
+++ src/Utilities/CreatePDF.java
@@ -1,8 +1,10 @@
 package utilities;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.apache.pdfbox.pdmodel.*;
+
 public class CreatePDF {
 
   public void pdfCreate() throws IOException {
@@ -12,16 +14,17 @@ public class CreatePDF {
     PDPage page = new PDPage();
     document.addPage(page);
     PDPageContentStream contentStream = new PDPageContentStream(document, page);
-    
+
     factura.crearFactura(contentStream);
 
-    document.save("D:/my_doc.pdf");
-    
     document.close();
   }
 
-  public void urlFactura(PDDocument document) throws IOException{
-    String curDir = System.getProperty("user.dir");
-    document.save(curDir + "/src/resources/facturas/factura.pdf");
+  public void urlFactura(PDDocument document) throws IOException {
+    String curDir = System.getProperty("user.dir") + "/src/resources/facturas/factura.pdf";
+    File f = new File(curDir);
+    f.delete();
+
+    document.save(curDir);
   }
 }
