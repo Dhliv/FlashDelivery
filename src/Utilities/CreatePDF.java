5c47a14b2a1bb439b23d556ffa51f5461ab34723
perqueza72 <practica22correo@gmail.com>
2021-05-28 10:49:15 -0500

    Modificacion

--- src/Utilities/CreatePDF.java
+++ src/Utilities/CreatePDF.java
@@ -8,20 +8,20 @@ public class CreatePDF {
   public void pdfCreate() throws IOException {
     FacturaContenido factura = new FacturaContenido();
     PDDocument document = new PDDocument();
-    urlFactura(document);
     PDPage page = new PDPage();
     document.addPage(page);
     PDPageContentStream contentStream = new PDPageContentStream(document, page);
     
     factura.crearFactura(contentStream);
 
-    document.save("D:/my_doc.pdf");
+    document.save(urlFactura(document));
     
     document.close();
   }
 
-  public void urlFactura(PDDocument document) throws IOException{
+  public String urlFactura(PDDocument document) throws IOException{
     String curDir = System.getProperty("user.dir");
-    document.save(curDir + "/src/resources/facturas/factura.pdf");
+    curDir = curDir + "/src/resources/facturas/factura.pdf";
+    return curDir;
   }
 }
