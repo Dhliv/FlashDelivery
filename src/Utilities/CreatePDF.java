986d732e20e78d0a5957977d345b64b4291ee2ec
perqueza72 <practica22correo@gmail.com>
2021-05-28 10:53:39 -0500

    Organizar metodos

--- src/Utilities/CreatePDF.java
+++ src/Utilities/CreatePDF.java
@@ -6,16 +6,14 @@ import org.apache.pdfbox.pdmodel.*;
 public class CreatePDF {
 
   public void pdfCreate() throws IOException {
-    FacturaContenido factura = new FacturaContenido();
     PDDocument document = new PDDocument();
     PDPage page = new PDPage();
     document.addPage(page);
-    PDPageContentStream contentStream = new PDPageContentStream(document, page);
-    
-    factura.crearFactura(contentStream);
+    FacturaContenido factura = new FacturaContenido(document,page);
+    factura.crearFactura();
 
-    document.save(urlFactura(document));
     
+    document.save(urlFactura(document));
     document.close();
   }
 
--- src/Utilities/FacturaContenido.java
+++ src/Utilities/FacturaContenido.java
@@ -7,7 +7,16 @@ import org.apache.pdfbox.pdmodel.*;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 public class FacturaContenido {
-  public void crearFactura(PDPageContentStream contentStream) throws IOException{
+
+  PDDocument document;
+  PDPage page;
+
+  public FacturaContenido(PDDocument document, PDPage page){
+    this.document = document;
+    this.page = page;
+  }
+  public void crearFactura() throws IOException{
+    PDPageContentStream contentStream = new PDPageContentStream(document, document.getPage(0));
     contentStream.beginText();
       contentStream.newLineAtOffset(25, 700);
       contentStream.setFont( PDType1Font.TIMES_ROMAN, 12 );
