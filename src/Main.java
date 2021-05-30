89e0f7e5fa455c873e4aba64bf9b81a69d0366c4
Dhliv <david.henao.martinez@correounivalle.edu.co>
2021-05-29 20:52:51 -0500

    .

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,7 @@ public class Main extends Application
         Globals.init(this);
         Globals.pantalla = stage;
         Ventana login = new Ventana("login", new Login());
-        login.start(stage);
+        login.start(stage);*/
     }
 
 }
