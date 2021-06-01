cbe40520964a8470b0e479092a40c2d57fa688d3
ReynellQ <73651961+ReynellQ@users.noreply.github.com>
2021-06-01 14:24:07 -0500

    librerias y cambios para generación de facturas

--- src/Main.java
+++ src/Main.java
@@ -10,35 +10,38 @@ import utilities.CreatePDF;
 import utilities.Globals;
 import utilities.Ventana;
 
-public class Main extends Application 
-{
+public class Main extends Application {
     public static void main(String[] args) {
         System.getProperties().setProperty("org.jooq.no-logo", "true"); // no mostrar logo de jOOQ
         launch(args);
     }
 
-    //! Borrar 
-    public void generatePDF() throws IOException{
+    // ! Borrar
+    public void generatePDF() throws IOException {
         String[][] infoPaq = new String[10][2];
         infoPaq[0][0] = "Descripción";
         infoPaq[0][1] = "Precio (COP)";
-        for(int i=1; i<infoPaq.length; i++){ 
+        for (int i = 1; i < infoPaq.length; i++) {
             infoPaq[i][0] = "Paquete" + Integer.toString(i);
             infoPaq[i][1] = Integer.toString((new Random(10)).nextInt());
         }
-        String[] infoDest = {"Nombre","Cedula","Direccion","Telefono", "Email"};  //INFORMACION DEL CLIENTE DESTINATARIO
-        String[] infoPago = {Date.valueOf(LocalDate.now()).toString(),"160000","150","27000","135000"};  //INFORMACION DEL PAGO
-        CreatePDF pdf = new CreatePDF(infoPaq,infoDest,infoDest, infoPago);
+        String[] infoDest = { "Nombre", "Cedula", "Direccion", "Telefono", "Email" }; // INFORMACION DEL CLIENTE
+                                                                                      // DESTINATARIO
+        String[] infoPago = { Date.valueOf(LocalDate.now()).toString(), "160000", "150", "27000", "135000" }; // INFORMACION
+                                                                                                              // DEL
+                                                                                                              // PAGO
+        CreatePDF pdf = new CreatePDF(infoPaq, infoDest, infoDest, infoPago);
         pdf.pdfCreate();
     }
 
-
     public void start(Stage stage) throws Exception {
-        generatePDF();
-        // Globals.init(this);
-        // Globals.pantalla = stage;
-        // Ventana login = new Ventana("login", new Login());
-        // login.start(stage);
+        // generatePDF();
+
+        Globals.init(this);
+        Globals.pantalla = stage;
+        Ventana login = new Ventana("login", new Login());
+        login.start(stage);
+
     }
 
 }
