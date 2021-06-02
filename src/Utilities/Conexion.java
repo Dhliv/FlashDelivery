556256316d72c0a7a6f68485f846545cea22f15c
Dhliv <david.henao.martinez@correounivalle.edu.co>
2021-06-01 21:27:39 -0500

    Correción de método que accedía a la BD directly.

--- src/Utilities/Conexion.java
+++ src/Utilities/Conexion.java
@@ -13,29 +13,11 @@ import java.io.IOException;
 public class Conexion {
     private static Connection conn = null;
 
+    /**
+     * Arreglo temporal para la eliminación futura de este método.
+     */
     public static Connection startConnection() {
-        try {
-            if (conn == null) {
-                // Determina cuando se termina el programa
-                // Runtime.getRuntime().addShutdownHook(new MiShDwnHook());
-                // Recupera los parámetros de conexión del archivo
-                // jdbc.properties
-
-                // ResourceBundle resource = ResourceBundle.getBundle("servicios.jdbc");
-                String driver = "org.postgresql.Driver";
-                // resource.getString("driver");
-                String url = "jdbc:postgresql://ec2-52-87-107-83.compute-1.amazonaws.com:5432/d622m7j3h054ts";
-                // resource.getString("url");
-                String usr = "ikwnggozhnxhvp";
-                String pwd = "a933d68a3c21b7b24a2e05104117b487091c7b880a72fe25f4ae721fadbbae9a";
-
-                Class.forName(driver);
-                conn = DriverManager.getConnection(url, usr, pwd);
-            }
-
-        } catch (ClassNotFoundException | SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Error : " + ex.toString());
-        }
+        db();
         return conn;
     }
 
@@ -59,7 +41,7 @@ public class Conexion {
     public static DSLContext db() {
         try {
             Properties dbs = new Properties();
-            dbs.load(new FileReader("resources/db.properties"));
+            dbs.load(new FileReader("src/resources/db.properties"));
             String url = dbs.getProperty("url");
             String usr = dbs.getProperty("usr");
             String pwd = dbs.getProperty("pwd");
