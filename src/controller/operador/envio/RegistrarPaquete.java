388c215fcf39df211c9e47d310cd788b05a75915
ReynellQ <73651961+ReynellQ@users.noreply.github.com>
2021-09-20 22:36:56 -0500

    cambio vista paquetes

--- src/controller/operador/envio/RegistrarPaquete.java
+++ src/controller/operador/envio/RegistrarPaquete.java
@@ -24,32 +24,33 @@ public class RegistrarPaquete {
 
   @FXML
   private Button atrasPaquete;
+
   @FXML
   private Button btRegistrarEnvios;
-  @FXML
-  private Button btInsertar;
-  @FXML
-  private Button btBorrar;
+
   @FXML
   private TextArea txtDescripcion;
+
   @FXML
   private TextField txtAncho;
+
   @FXML
   private TextField txtAlto;
+
   @FXML
   private TextField txtLargo;
-  @FXML
-  private Button btModificar;
+
   @FXML
   private CheckBox checkSeguro;
+
   @FXML
   private TextField txtPeso;
+
   @FXML
   private TextField txtValor;
-  @FXML
-  private TextArea txtReporte;
 
-  private boolean modify; // Boolean que indica si se está modificando un paquete o no
+  @FXML
+  private Button btBorrar;
 
   private model.RegistrarEnvio envio;
   private Empleado operador;
@@ -65,8 +66,6 @@ public class RegistrarPaquete {
     TextFieldRestrictions.textFieldNumeric(txtAncho);
     TextFieldRestrictions.textFieldNumeric(txtLargo);
     TextFieldRestrictions.textFieldNumeric(txtAlto);
-    //txtReporte.setText("xdxdxd");
-    modify = false;
   }
 
   @FXML
@@ -84,56 +83,20 @@ public class RegistrarPaquete {
     txtAncho.setText("");
   }
 
-  @FXML
-  void borrarPaquete(ActionEvent event) {
-    envio.eliminarPaquete();
-    clearFieldsPaquetes();
-  }
 
-  @FXML
-  void insertarPaquete(ActionEvent event) {
-    if (agregarPaquete()) {
-      JOptionPane.showMessageDialog(null, "El paquete ha sido ingresado");
-    } else {
-      SpecificAlerts.showErrorUnexpt();
-    }
-  }
-
-  @FXML
-  void modificarPaquete(ActionEvent event) {
-    if (modify) {
-      if (agregarPaquete()) {
-        JOptionPane.showMessageDialog(null, "El paquete ha sido modificado");
-      } else {
-        modify = false;
-        btBorrar.setText("Modificar paquete");
-      }
-    } else {
-      Paquete p = envio.getPaquete();
-      txtPeso.setText(p.peso + "");
-      txtValor.setText(p.valor + "");
-      txtDescripcion.setText(p.descripcion);
-      txtAncho.setText(p.ancho + "");
-      txtLargo.setText(p.largo + "");
-      txtAlto.setText(p.alto + "");
-      checkSeguro.setSelected(p.seguro);
-      btBorrar.setText("Confirmar modificación");
-      modify = true;
-    }
-
-  }
 
   boolean agregarPaquete() {
     try { // Faltan validaciones
       Integer peso = Integer.parseInt(txtPeso.getText());
       Integer valor = Integer.parseInt(txtValor.getText());
       String descripcion = txtDescripcion.getText();
+      if (descripcion.trim().equals(""))
+        return false;
       Integer ancho = Integer.parseInt(txtAncho.getText());
       Integer largo = Integer.parseInt(txtLargo.getText());
       Integer alto = Integer.parseInt(txtAlto.getText());
       Boolean seguro = checkSeguro.isSelected();
       envio.agregarPaqueteP(peso, valor, descripcion, ancho, largo, alto, seguro);
-      clearFieldsPaquetes();
       return true;
     } catch (NumberFormatException e) {
       return false;
@@ -142,14 +105,19 @@ public class RegistrarPaquete {
 
   @FXML
   void resumenEnvio(ActionEvent event) {
-    if (envio.getPaquete() == null || modify)
+
+    if (!agregarPaquete())
       JOptionPane.showMessageDialog(null, "No ha ingresado ningún paquete");
     else
       Globals.cambiarVista("operador.resumen", new OperadorResumen(envio, operador));
 
   }
   @FXML
-    void superPrueba(KeyEvent event) {
-      System.out.println("Será que esto sí funciona así?");
-    }
+  void limpiarCampos(ActionEvent event) {
+    clearFieldsPaquetes();
+  }
+  @FXML
+  void superPrueba(KeyEvent event) {
+    System.out.println("Será que esto sí funciona así?");
+  }
 }
