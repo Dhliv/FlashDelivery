0a4ff2f2bc7c3c62c07f9e293fea2bcb53df8c26
David Henao <david.henao.martinez@correounivalle.edu.co>
2021-09-22 16:12:39 -0500

    .

--- src/controller/gerente/usuarios/UserConsulta.java
+++ src/controller/gerente/usuarios/UserConsulta.java
@@ -10,7 +10,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
