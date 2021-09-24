697c29120093310cbdf7c6b9d8d0ce3f0673d02a
perqueza72 <practica22correo@gmail.com>
2021-09-23 20:22:40 -0500

    Mejoras visuales en operadorOficina

--- src/controller/operador/OperadorOficina.java
+++ src/controller/operador/OperadorOficina.java
@@ -30,6 +30,9 @@ public class OperadorOficina {
   public void initialize() {
     View.setViewPane(rightContent, true);
     View.cambiar("operadorOficinaTabla", new OperadorConsulta(operador));
+    
+    labelUsuario.setText("Bienvenido " + operador.getNombres()); 
+    labelPOS.setText("Punto de venta " + operador.getSede() );
     // View.cambiar("operador.cliente", new RegistrarClientes(operador));
   }
 
