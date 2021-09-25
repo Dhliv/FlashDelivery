ed51e52b3a075977706d62148ae4a18aac81f49e
perqueza72 <practica22correo@gmail.com>
2021-09-25 18:00:14 -0500

    Inserción de datos en Recoger paquetes.
    Co-authored-by: Dhliv <Dhliv@users.noreply.github.com>

--- src/controller/operador/envio/RecogerPaquete.java
+++ src/controller/operador/envio/RecogerPaquete.java
@@ -1,9 +1,8 @@
 package controller.operador.envio;
 
+import model.Entities.Cliente;
 import java.util.List;
-
 import javax.swing.JOptionPane;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -16,13 +15,15 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+
 import model.Entities.Empleado;
 import model.Entities.Sede;
-import utilities.Roles;
 
+import utilities.Roles;
+import utilities.GeneralChecker;
 import utilities.TextFieldRestrictions;
 import utilities.View;
-
+import utilities.SpecificAlerts;
 /**
  * Controlador de la vista operador.recoger
  * 
@@ -69,6 +70,8 @@ public class RecogerPaquete {
     choiceSede.setValue(sedes.get(0).toString());
 
     auxiliares.removeAll(auxiliares);
+    auxiliares.addAll(Empleado.getAuxiliaresByIdParsed(Sede.getSedes().get(0).getId()));
+    choiceAuxiliar.getItems().addAll(auxiliares);
   }
 
   @FXML
@@ -77,7 +80,7 @@ public class RecogerPaquete {
 
     ObservableList<String> auxiliares = FXCollections.observableArrayList();
     auxiliares.removeAll(auxiliares);
-    auxiliares = Empleado.getAuxiliaresByIdParsed(idSede);
+    auxiliares.addAll(Empleado.getAuxiliaresByIdParsed(idSede));
     choiceAuxiliar.getItems().addAll(auxiliares);
   }
 
@@ -93,13 +96,40 @@ public class RecogerPaquete {
     goBack();
   }
 
-  // TODO INSERTAR DATOS EN BD
+  /**
+   * Valida los datos obtenidos de los campos rellenables verificando que no haya campos sin llenar o que existan caracteres vacíos.
+   * @return True si cumple las validaciones y no hay problemas, False de lo contrario.
+   */
+  private Boolean validateData(){
+    String desc = txtAreaDescripcion.getText();
+    String cedula = choiceAuxiliar.getValue() == null ? "" : Empleado.getCedulaAuxiliar(choiceAuxiliar.getValue());
+    String idSede = choiceSede.getValue() == null ? "" : Integer.toString(Sede.getIdSede(choiceSede.getValue()));
+
+    String[] campos = {desc, cedula, idSede};
+    boolean camposVacios = GeneralChecker.checkEmpty(campos, new Object[0]);
+    boolean forbiddenChar = GeneralChecker.checkChar(campos);
+
+    if(camposVacios || forbiddenChar){
+      if(camposVacios)
+        SpecificAlerts.showEmptyFieldAlert();
+      if(forbiddenChar)
+        SpecificAlerts.showCharForbidenAlert();
+      return Boolean.FALSE;
+    }else return Boolean.TRUE;
+  }
+
   public void insertData() {
-    txtAreaDescripcion.getText();
-    getCedulaAuxiliar(choiceAuxiliar.getValue());
-    choiceSede.getValue();
-    envio.getDestinatario();
-    envio.getRemitente();
+    if(!validateData())
+      return;
+    
+    String descripcion = txtAreaDescripcion.getText();
+    // String cedulaAuxiliar = Empleado.getCedulaAuxiliar(choiceAuxiliar.getValue());
+    // int idSede = Sede.getIdSede(choiceSede.getValue());
+
+    Cliente.createCliente(envio.getRemitente());
+    Cliente.createCliente(envio.getDestinatario());
+    model.Peticion.createPeticion(descripcion, operador.getCedula(), envio.getRemitente().cedula, envio.getDestinatario().cedula);
+  
   }
 
   private void goBack() {
