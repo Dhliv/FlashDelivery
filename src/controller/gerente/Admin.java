af41eeb473b776a8859db1fae6a61c3e685cd851
David Henao <david.henao.martinez@correounivalle.edu.co>
2021-09-25 11:34:39 -0500

    Se agrega TODO y se elimina un método no usado.

--- src/controller/gerente/Admin.java
+++ src/controller/gerente/Admin.java
@@ -10,6 +10,7 @@ import javafx.scene.layout.AnchorPane;
 import utilities.Globals;
 import utilities.View;
 
+//TODO Documentar y cambiar métodos visuales de Globals a View.
 /**
  * Controlador de la vista usada para el Admin
  * 
@@ -23,35 +24,36 @@ public class Admin {
     this.userName = userName;
   }
 
-  @FXML private Label labelNameUser;
+  @FXML
+  private Label labelNameUser;
 
-  @FXML private AnchorPane content;
+  @FXML
+  private AnchorPane content;
 
-  @FXML private void initialize() {
+  @FXML
+  private void initialize() {
     View.setViewPane(content, false);
     Globals.viewPane = content;
     labelNameUser.setText("Bienvenido " + userName);
   }
 
-  @FXML void goToSedeConsulta(ActionEvent event) {
+  @FXML
+  void goToSedeConsulta(ActionEvent event) {
     Globals.cambiarVista("sede.consulta");
   }
 
-  @FXML void goToUsuariosConsulta(ActionEvent event) {
-    Globals.cambiarVista("user.consulta", new UserConsulta());
+  @FXML
+  void goToUsuariosConsulta(ActionEvent event) {
+    View.newView("user.consulta", new UserConsulta());
   }
 
-  @FXML void goToAdminReportes(ActionEvent event) {
+  @FXML
+  void goToAdminReportes(ActionEvent event) {
     Globals.cambiarVista("reportes");
   }
 
-  // TODO eliminar usuarios.
-  @FXML void borrar(ActionEvent event) {
-    String user = "juanito";
-    JOptionPane.showOptionDialog(null, "Desea borrar a " + user + "?", "Eliminar registro", 0, 0, null, null, user);
-  }
-
-  @FXML void logOut(ActionEvent event) {
+  @FXML
+  void logOut(ActionEvent event) {
     Globals.logOut();
   }
 }
