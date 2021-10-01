988d5dcc08919d1d34aae5f30e293cde07e7d6ea
perqueza72 <practica22correo@gmail.com>
2021-10-01 17:13:55 -0500

    Se agrega funcionalidad al requerir el informe medioDePago

--- src/controller/gerente/CreateChart.java
+++ src/controller/gerente/CreateChart.java
@@ -1,25 +1,46 @@
 package controller.gerente;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Entities.Sede;
+import utilities.View;
+
 public class CreateChart {
-  public static Object[] medioDePago(){
-    String[] informe = {"Dinero Mensual","Mes","Dinero"};
-    String[] fecha = {"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
-    String[] sedeNombre = {"Sede 1", "Sede 2"};
-    Number[][] sedeInformacion = {{1,2,3,4,5,6,7,8,9,10,11,12},{1,2,3,4,5,6,7,8,9,10,11,12}};
-    Object[] info = {informe, fecha, sedeNombre, sedeInformacion};
-    return info;
+
+  private static String[] informe;
+  private static String[] fecha;
+  private static ArrayList<String> sedeNombre;
+  private static Number[][] sedeInformacion;
+  private static final String[] MESES = {"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
+
+  /**
+   * Carga los datos que necesita ReporteEmpresa para crear un Chart
+   * relacionada a los métodos de pago.
+   * @param sedeId  Lista con los IDs de las sedes solicitadas.
+   */
+  public static void medioDePago(ArrayList<Integer> sedeId){
+
+    informe = new String[]{"Dinero Mensual","Mes","Dinero"};
+    fecha = MESES;
+    sedeNombre =  new ArrayList<String>();
+    sedeInformacion = new Number[2][];
+
+    for(int i=0; i<sedeId.size(); i++){
+      sedeNombre.add(Sede.parseSede(sedeId.get(i)));                      //Agrega el nombre de la sede con ID "sedeId.get(i)"
+      sedeInformacion[i] = model.Reportes.getMedioDePago(sedeId.get(i));  //Agrega la información de la query de pago.
+    }
+    
+    View.newView("vacio.completo", new ReporteEmpresa(informe,fecha,sedeNombre.toArray(new String[0]),sedeInformacion));
   }
-  public static Object[] paquetesEnviados(){
-    return null;
+  public static void paquetesEnviados(){
+    
   }
-  public static Object[] servicioSolicitado(){
-    return null;
+  public static void servicioSolicitado(){
   }
-  public static Object[] ventasMensuales(){
-    return null;
+  public static void ventasMensuales(){
   }
-  public static Object[] ventasSemanales(){
-    return null;
+  public static void ventasSemanales(){
   }
   
 }
